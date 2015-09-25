f117b5279eef2117ae86a0124b93a7abd8290923
Noel Farris <noel.farris@gmail.com>
2015-09-25 14:59:28 -0500

    inital commit to job submit branch

---
+++ scripts/views/jobSubmitView.js
@@ -0,0 +1,12 @@
+var Backbone = require('backbone');
+var $ = require('jquery');
+var _ = require('backbone/node_modules/underscore');
+var JobsCollection = require('../collections/jobsCollection.js')
+
+module.exports = Backbone.View.extend({
+
+
+
+
+
+})
