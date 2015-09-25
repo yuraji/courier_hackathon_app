90e883bef9a34a2127bb5be0df46e8fb943e5319
Noel Farris <noel.farris@gmail.com>
2015-09-25 12:30:06 -0500

    created view model and collection and added jobs section in HTML

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+var Backbone = require('backbone');
+var $ = require('jquery');
+var _ = require('backbone/node_modules/underscore');
+var JobsCollection = require('./collections/jobsCollection.js');
+var JobsView = require('./views/jobsView.js');
+
+$(document).ready(function() {
+
+
+
+new JobsView();
+})
